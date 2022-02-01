de10d5c865663353b63c25287feb7abe55fe935d
renenennene <rene@beiermann.net>
2022-02-01 20:41:06 +0100

    Test

---
+++ app/src/main/java/com/example/flappybird/SplashActivtiy.java
@@ -0,0 +1,26 @@
+package com.example.flappybird;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashActivtiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_activtiy);
+
+        View view = findViewById(R.id.backg);
+
+
+
+
+    }
+}
