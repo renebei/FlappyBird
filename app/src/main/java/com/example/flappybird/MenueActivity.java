13b91c2cb6159ee4dc4eee9a15cf316c642db51b
rene <rene.beiermann@hs-osnabrueck.de>
2022-02-02 02:35:03 +0100

    Menü

--- app/src/main/java/com/example/flappybird/MenueActivity.java
+++ app/src/main/java/com/example/flappybird/MenueActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 public class MenueActivity extends AppCompatActivity {
@@ -15,6 +17,8 @@ public class MenueActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_activtiy);
         this.background = findViewById(R.id.backg);
         this.playButton = findViewById(R.id.playbutton);
